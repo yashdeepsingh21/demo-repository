428704f57ea220df12177e0b7f3586d2587ef450
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-20 16:52:23 +0530

    fixed bug successfully

--- asyncio_test.py
+++ asyncio_test.py
@@ -4,13 +4,15 @@ import time
 
 async def asset_function(delay):
     print("this is an asset function for other functions")
-    time.sleep(delay)
+    if delay:
+        time.sleep(delay)
+        return delay
     return None
 
 
 async def dependent_function():
     print("this function is dependent on another function")
-    money = await asset_function(10)
+    money = await asset_function(delay = None)
     if money:
         print("asset function provide money")
     else:
