65670f3fdcc985d99290334f0d47c6c9bdbb6feb
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-20 16:29:08 +0530

    asyncio function demo

--- demo.py
+++ demo.py
@@ -1,3 +1,28 @@
-print("Yashdeep Singh")
+# print("Yashdeep Singh")
+#
+# print("Python Developer")
 
-print("Python Developer")
+
+import asyncio
+import time
+
+
+async def execute(delay, value):
+    await asyncio.sleep(delay)
+    print(value)
+    return value
+
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    data1 = await execute(1, 'hello')
+    if data1:
+        await execute(20, 'first function executed after 20 seconds')
+    else:
+        print("something went wrong")
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+asyncio.run(main())
