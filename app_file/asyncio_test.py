40d0abb2d824fb7f2d2a8d5ef30b09a538405b6f
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-20 17:09:48 +0530

    remove the unwanted spaces on line 15

--- app_file/asyncio_test.py
+++ app_file/asyncio_test.py
@@ -12,7 +12,7 @@ async def asset_function(delay):
 
 async def dependent_function():
     print("this function is dependent on another function")
-    money = await asset_function(delay = None)
+    money = await asset_function(delay=None)
     if money:
         print("asset function provide money")
     else:
