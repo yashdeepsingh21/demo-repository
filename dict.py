4463a93c403e88d5b2fc3b500837354b4c45bf76
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-21 14:49:24 +0530

    new dict dsa question added

--- dict.py
+++ dict.py
@@ -33,11 +33,43 @@
 
 # Python – Inversion in nested dictionary ~~~~important~~~~
 
-test_dict = {'a': {'b': {}}, 'd': {'e': {}}, 'f': {'g': {}}}
+# test_dict = {'a': {'b': {}}, 'd': {'e': {}}, 'f': {'g': {}}}
+#
+# result_dict = {}
+#
+# for key, value in test_dict.items():
+#     result_dict[list(value.keys())[0]] = {key: list(value.values())[0]}
+#
+# print(result_dict)
 
-result_dict = {}
+# Python – Extract Key’s Value, if Key Present in List and Dictionary
 
-for key, value in test_dict.items():
-    result_dict[list(value.keys())[0]] = {key: list(value.values())[0]}
+test_list = ["Gfg", "is", "Good", "for", "Geeks"]
+test_dict = {"Gf": 5, "Best": 6}
+K = "Gfg"
 
-print(result_dict)
+
+def get_keys_value(test_list, test_dict, K):
+    test_list = test_list
+    test_dict = test_dict
+    K = K
+    result = None
+    for value in test_list:
+        if K == value:
+            test_list = True
+            break
+
+    if test_list == True:
+        for key in test_dict.keys():
+            if K == key:
+                result = test_dict[K]
+                break
+        if result:
+            return f"{K} is present in list and has value {result}"
+        else:
+            return f"{K} is present in list but not in dict"
+    else:
+        return f"{K} is not present in list"
+
+
+print(get_keys_value(test_list, test_dict, K))
