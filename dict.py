c9a1cb18568741ae824263f40155bdbd30c2bd71
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-21 15:16:00 +0530

    new question of dict added

--- dict.py
+++ dict.py
@@ -43,33 +43,54 @@
 # print(result_dict)
 
 # Python – Extract Key’s Value, if Key Present in List and Dictionary
+#
+# test_list = ["Gfg", "is", "Good", "for", "Geeks"]
+# test_dict = {"Gf": 5, "Best": 6}
+# K = "Gfg"
+#
+#
+# def get_keys_value(test_list, test_dict, K):
+#     test_list = test_list
+#     test_dict = test_dict
+#     K = K
+#     result = None
+#     for value in test_list:
+#         if K == value:
+#             test_list = True
+#             break
+#
+#     if test_list == True:
+#         for key in test_dict.keys():
+#             if K == key:
+#                 result = test_dict[K]
+#                 break
+#         if result:
+#             return f"{K} is present in list and has value {result}"
+#         else:
+#             return f"{K} is present in list but not in dict"
+#     else:
+#         return f"{K} is not present in list"
+#
+#
+# print(get_keys_value(test_list, test_dict, K))
 
-test_list = ["Gfg", "is", "Good", "for", "Geeks"]
-test_dict = {"Gf": 5, "Best": 6}
-K = "Gfg"
+
+# Python – Remove keys with Values Greater than K ( Including mixed values )
+
+test_dict = {'Gfg': 3, 'is': 7, 'best': 10, 'for': 6, 'geeks': 'CS'}
+
+K = 6
 
 
-def get_keys_value(test_list, test_dict, K):
-    test_list = test_list
-    test_dict = test_dict
-    K = K
-    result = None
-    for value in test_list:
-        if K == value:
-            test_list = True
-            break
-
-    if test_list == True:
-        for key in test_dict.keys():
-            if K == key:
-                result = test_dict[K]
-                break
-        if result:
-            return f"{K} is present in list and has value {result}"
-        else:
-            return f"{K} is present in list but not in dict"
-    else:
-        return f"{K} is not present in list"
+def remove_key_greater_than_K(test_dict, K):
+    result = {}
+    for key, value in test_dict.items():
+        print(type(value), value)
+        if type(value) != type(K):
+            result[key] = value
+        elif value <= K:
+            result[key] = value
+    return result
 
 
-print(get_keys_value(test_list, test_dict, K))
+print(remove_key_greater_than_K(test_dict, K))
