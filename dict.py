947debb072fb4f43ed01760f9230d56a974df137
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-21 13:00:01 +0530

    problem 2 on dict-dsa added

--- dict.py
+++ dict.py
@@ -1,12 +1,32 @@
 # problem number one
+#
+# test_dict = {'Gfg': 4, 'best': 9}
+# test_list = [8, 2]
+#
+# result_dict = {}
+# count = 0
+# for key, value in test_dict.items():
+#     result_dict[test_list[count]] = {key: value}
+#     count += 1
+#
+# print(result_dict)
 
-test_dict = {'Gfg': 4, 'best': 9}
-test_list = [8, 2]
+# Python – Swapping Hierarchy in Nested Dictionaries
 
-result_dict = {}
-count = 0
-for key, value in test_dict.items():
-    result_dict[test_list[count]] = {key: value}
-    count += 1
-
-print(result_dict)
+# test_dict = {'Gfg': {'best' : [1, 3, 4]}}
+#
+# result_dict = {}
+#
+# key = None
+#
+# value = None
+#
+# for keys , values in test_dict.items():
+#     key = keys
+#     value = values
+#     break
+#
+# for keys, values in value.items():
+#     result_dict[keys] = {key: values}
+#
+# print(result_dict)
