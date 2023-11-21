1c44deb069dc217b92c1e9f56fbf1422b55008f5
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-11-21 11:43:35 +0530

    conflicts resolve

--- app_file/asyncio_test.py
+++ app_file/asyncio_test.py
@@ -12,11 +12,7 @@ async def asset_function(delay):
 
 async def dependent_function():
     print("this function is dependent on another function")
-<<<<<<< HEAD:app_file/asyncio_test.py
-    money = await asset_function(delay=None)  # change delay time accordingly
-=======
     money = await asset_function(delay=None)
->>>>>>> 40d0abb2d824fb7f2d2a8d5ef30b09a538405b6f:app_file/asyncio_test.py
     if money:
         print("asset function provide money")
     else:
